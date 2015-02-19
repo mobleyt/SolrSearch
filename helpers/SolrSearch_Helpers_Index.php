ce4f745187619e00635ea1dac5ed867c2b4993bc
Tyler Mobley <mobleyt@cofc.edu>
2015-02-19 14:25:55 -0500

    Update SolrSearch_Helpers_Index.php
    add fields for solr index

--- helpers/SolrSearch_Helpers_Index.php
+++ helpers/SolrSearch_Helpers_Index.php
@@ -86,12 +86,13 @@ class SolrSearch_Helpers_Index
         $doc->setField('model', 'Item');
         $doc->setField('modelid', $item->id);
 
-        // extend $doc to to include and items public / private status
-        $doc->setField('public', $item->public);
-
         // Title:
-        $title = metadata($item, array('Dublin Core', 'Title'));
+        $title = metadata($item, array('MODS', 'Title'));
         $doc->setField('title', $title);
+
+        // Date:
+        $date = metadata($item, array('MODS', 'Origin Info - Date Created'));
+        $doc->setField('date', $date);
 
         // Elements:
         self::indexItem($fields, $item, $doc);
@@ -225,9 +226,7 @@ class SolrSearch_Helpers_Index
         $table  = $db->getTable('Item');
         $select = $table->getSelect();
 
-        // Removed in order to index both public and private items
-        // $table->filterByPublic($select, true);
-
+        $table->filterByPublic($select, true);
         $table->applySorting($select, 'id', 'ASC');
 
         $excTable = $db->getTable('SolrSearchExclude');
@@ -235,12 +234,11 @@ class SolrSearch_Helpers_Index
         foreach ($excTable->findAll() as $e) {
             $excludes[] = $e->collection_id;
         }
-        if (!empty($excludes)) {
+		if (!empty($excludes)) {
             $select->where(
                 'collection_id IS NULL OR collection_id NOT IN (?)',
                 $excludes);
         }
-
         // First get the items.
         $pager = new SolrSearch_DbPager($db, $table, $select);
         while ($items = $pager->next()) {
